1516e55c1e2f09dee135f0fafd87731d5af932ae
coafiori010 <coa_fiori_01@hotmail.com>
2020-05-29 20:43:45 +0000

    prueba commit 2

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast"
 ], function (Controller, MessageToast) {
-	"use strict";
+	"use strict"; 
 
 	return Controller.extend("logaligroup.SAPUI5.controller.App", {
 
