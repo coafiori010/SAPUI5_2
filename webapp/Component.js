bf820c1321c7aa20d8fc3a1cdbe47a91cdfd3a61
coafiori010 <coa_fiori_01@hotmail.com>
2020-08-19 19:05:53 +0000

    2020.08.19

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,30 +1,41 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"logaligroup/SAPUI5/model/models",
-	"sap/ui/model/resource/ResourceModel"
-	], function (UIComponent, models, ResourceModel){
+	"sap/ui/model/resource/ResourceModel",
+	"./controller/HelloDialog"
+], function (UIComponent, models, ResourceModel, HelloDialog) {
 
-		return UIComponent.extend("logaligroup.SAPUI5.Component",{
-		 
-			metadata: {
-				manifest: "json"
-			},
+	return UIComponent.extend("logaligroup.SAPUI5.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		init: function () {
+			// Call the Init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			//set model on view
+			this.setModel(models.createRecipient());
+
+			//set i18n model on view
+			var i18nModel = new ResourceModel({
+				bundleName: "logaligroup.SAPUI5.i18n.i18n"
+			});
+			this.setModel(i18nModel, "i18n");
+
+			this._helloDialog = new HelloDialog(this.getRootControl());
+
+		},
+
+		exit: function () {
+			this.helloDialog.destroy();
+			delete this._helloDialog;
+		},
 		
-			init: function(){
-				// Call the Init function of the parent
-				UIComponent.prototype.init.apply(this, arguments);
-				
-				//set model on view
-				this.setModel(models.createRecipient());
-				
-				//set i18n model on view
-				var i18nModel = new ResourceModel({
-					bundleName: "logaligroup.SAPUI5.i18n.i18n"
-				});
-				this.setModel(i18nModel, "i18n");
-			}
-			
-		}); 
-		
-	}
-	);
+		openHelloDialog: function(){
+			this._helloDialog.open();
+		}
+	});
+
+});
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,22 +2,17 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast"
 ], function (Controller, MessageToast) {
-	"use strict"; 
+	"use strict";
 
 	return Controller.extend("logaligroup.SAPUI5.controller.App", {
 
 		onInit: function () {
-			
+
 		},
-
-		onShowHello: function () {
-			
-			// read text from i18n / model
-			var sHello = this.getView().getModel("i18n").getResourceBundle().getText("sayHello");
-			var sName = this.getView().getModel().getProperty("/recipient/name");
-			var sMsg = sHello.concat(" ").concat(sName);
-			MessageToast.show(sMsg);
+		
+		onOpenHeader: function(){
+			this.getOwnerComponent().openHelloDialog();
 		}
-
+		
 	});
 });
